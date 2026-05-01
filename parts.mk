3e54638dae7015b53fd62e23d17f8a1ac99dc56d
zenin1504 <kayozzprojects@yandex.com>
2026-05-01 14:29:09 +0800

    parts: Initial repository for XiaomiParts
    Change-Id: I108ae0dcfe204677b1cd96d066176dc6
    Signed-off-by: zenin1504 <kayozzprojects@yandex.com>

---
+++ parts.mk
@@ -0,0 +1,24 @@
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Parts
+PARTS_PATH := packages/apps/XiaomiParts
+
+# Properties
+TARGET_VENDOR_PROP += $(PARTS_PATH)/vendor.prop
+
+# Soong Namespace
+PRODUCT_SOONG_NAMESPACES += \
+   $(PARTS_PATH)
+
+# Sepolicy
+BOARD_VENDOR_SEPOLICY_DIRS += $(PARTS_PATH)/sepolicy/vendor
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(PARTS_PATH)/sepolicy/private
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(PARTS_PATH)/sepolicy/public
+
+# XiaomiParts
+PRODUCT_PACKAGES += \
+    XiaomiParts
